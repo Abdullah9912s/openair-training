097975bc6a1bf0fb108bcb3fa8780064fc936046
sateesh <masabhathini@gmail.com>
2024-12-17 00:47:49 +0300

    Create day3_trajCluster.R

---
+++ day3_trajCluster.R
@@ -0,0 +1,16 @@
+#!/usr/bin/R
+sat <- read.csv('Manama_2016_2021_Raw.csv',header=TRUE,sep=',')
+sat$date <- as.POSIXct(strptime(sat$Date, format="%d/%m/%Y %H:%M"),tz="GMT")
+sat  <- subset(sat,select=c(date,O3))
+#sat$stn <- "Manama"
+save(sat,file="OzoneDataManama.Rdata")
+#######################
+load('5YManama2016to2021.RData')
+load('OzoneDataManama.Rdata')
+mas <- merge(sat,traj,  by = "date")
+
+#########################
+clust <- trajCluster(mas, method = "Euclid", n.cluster= 4, col = "increment")
+png("5y4ClusterOzoneCWT.png",width = 8*300, height = 2*300, res=150)
+trajLevel(subset(clust$data,lon > 0 & lon < 100 & lat > -20 & lat < 60), pollutant = "AOD500", type = "cluster",statistic="cwt",parameters=NULL,orientation=c(90,0,0),projection="mercator",cols = "increment",main="CWT_TRIV(AOD500)-2011_2015",map=TRUE,map.fill=FALSE,grid.col="gray",par.settings=list(fontsize=list(text=12)), layout = c(4, 1))
+dev.off()
