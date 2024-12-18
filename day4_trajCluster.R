051737799ed687683a204d13da96d7ee5e8125c7
sateesh <masabhathini@gmail.com>
2024-12-18 13:58:57 +0300

    Update day4_trajCluster.R

--- day4_trajCluster.R
+++ day4_trajCluster.R
@@ -12,5 +12,6 @@ mas <- merge(sat,traj,  by = "date")
 #########################
 clust <- trajCluster(mas, method = "Euclid", n.cluster= 4, col = "increment")
 png("5y4ClusterOzoneCWT.png",width = 8*300, height = 2*300, res=150)
-trajLevel(subset(clust$data,lon > 0 & lon < 100 & lat > -20 & lat < 60), pollutant = NOx", type = "cluster",statistic="cwt",parameters=NULL,orientation=c(90,0,0),projection="mercator",cols = "increment",main="CWT_TRIV(AOD500)-2011_2015",map=TRUE,map.fill=FALSE,grid.col="gray",par.settings=list(fontsize=list(text=12)), layout = c(4, 1))
+#trajLevel(subset(clust$data,lon > 0 & lon < 100 & lat > -20 & lat < 60), pollutant = NOx", type = "cluster",statistic="cwt",parameters=NULL,orientation=c(90,0,0),projection="mercator",cols = "increment",main="CWT_TRIV(AOD500)-2011_2015",map=TRUE,map.fill=FALSE,grid.col="gray",par.settings=list(fontsize=list(text=12)), layout = c(4, 1))
+trajLevel(mas, pollutant="O3", statistic="cwt") 
 dev.off()
