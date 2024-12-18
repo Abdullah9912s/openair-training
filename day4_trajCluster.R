f7f3ec9f71d950f5136c89871c25e757a3285067
sateesh <masabhathini@gmail.com>
2024-12-18 13:40:47 +0300

    Update day4_trajCluster.R

--- day4_trajCluster.R
+++ day4_trajCluster.R
@@ -1,5 +1,5 @@
 #!/usr/bin/R
-sat <- read.csv('Manama_2016_2021_Raw.csv',header=TRUE,sep=',')
+sat <- read.csv('My_2016_2021_Raw.csv',header=TRUE,sep=',')
 sat$date <- as.POSIXct(strptime(sat$Date, format="%d/%m/%Y %H:%M"),tz="GMT")
 sat  <- subset(sat,select=c(date,O3))
 #sat$stn <- "Manama"
@@ -12,5 +12,5 @@ mas <- merge(sat,traj,  by = "date")
 #########################
 clust <- trajCluster(mas, method = "Euclid", n.cluster= 4, col = "increment")
 png("5y4ClusterOzoneCWT.png",width = 8*300, height = 2*300, res=150)
-trajLevel(subset(clust$data,lon > 0 & lon < 100 & lat > -20 & lat < 60), pollutant = "AOD500", type = "cluster",statistic="cwt",parameters=NULL,orientation=c(90,0,0),projection="mercator",cols = "increment",main="CWT_TRIV(AOD500)-2011_2015",map=TRUE,map.fill=FALSE,grid.col="gray",par.settings=list(fontsize=list(text=12)), layout = c(4, 1))
+trajLevel(subset(clust$data,lon > 0 & lon < 100 & lat > -20 & lat < 60), pollutant = NOx", type = "cluster",statistic="cwt",parameters=NULL,orientation=c(90,0,0),projection="mercator",cols = "increment",main="CWT_TRIV(AOD500)-2011_2015",map=TRUE,map.fill=FALSE,grid.col="gray",par.settings=list(fontsize=list(text=12)), layout = c(4, 1))
 dev.off()
