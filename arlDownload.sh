e17fda6b4042a3902df5200fa0039a2cb8df7812
sateesh <masabhathini@gmail.com>
2024-12-18 10:30:23 +0300

    Update arlDownload.sh

--- arlDownload.sh
+++ arlDownload.sh
@@ -1,3 +1,5 @@
+ftp://arlftp.arlhq.noaa.gov/pub/archives/reanalysis/RP202201.gbl
+
 for n in {2015..2010}; do for m in {01..12}; do wget ftp://arlftp.arlhq.noaa.gov/pub/archives/reanalysis/RP${n}${m}.gbl; done; done
 
 #for high resolution.
